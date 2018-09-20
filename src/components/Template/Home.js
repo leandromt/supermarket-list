02539a44560cc9f73d39d6a11c75b909d5d3f7ed
Leandro Tavares <leandro.mendestavares@gmail.com>
2018-09-19 23:45:31 -0300

    Add Constructor and Functions Get List

--- src/components/Template/Home.js
+++ src/components/Template/Home.js
@@ -1,15 +1,27 @@
+// Dependencies
 import React, { Component } from "react";
 import socketIOClient from "socket.io-client";
+import axios from "axios";
 
+// Components
 import Header from "./Header";
 
+// Constantes
+const URL = "http://localhost:3000/categories";
+
 class Home extends Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
+
+    // Stats
     this.state = {
       response: false,
-      endpoint: "http://localhost:3001"
+      endpoint: "http://localhost:3001",
+      list: []
     };
+
+    // Initialize
+    this.getList();
   }
 
   componentDidMount() {
@@ -18,6 +30,16 @@ class Home extends Component {
     socket.on("CATEGORY_CREATED", data => this.setState({ response: data }));
     socket.on("CATEGORY_UPDATED", data => this.setState({ response: data }));
     socket.on("CATEGORY_DELETED", data => this.setState({ response: data }));
+  }
+
+  getList() {
+    console.log("getList");
+    axios.get(URL).then(resp =>
+      this.setState({
+        ...this.state,
+        list: resp.data
+      })
+    );
   }
 
   render() {
