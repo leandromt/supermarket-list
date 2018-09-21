fac1a73a37932db5020653aa3b1144ac9581b47a
Leandro Tavares <leandro.mendestavares@gmail.com>
2018-09-20 21:46:22 -0300

    Add Keyboard ENTER

--- src/components/template/Form.js
+++ src/components/template/Form.js
@@ -1,27 +1,35 @@
 import React from "react";
 import IconButton from "./IconButton";
 
-export default props => (
-  <div className="pb-2" role="form">
-    <div className="row">
-      <div className="col-9 col-md-10 col-lg-10 col-xl-10">
-        <input
-          id="description"
-          className="form-control"
-          placeholder="add item"
-          onChange={props.formChange}
-          value={props.description}
-        />
-      </div>
+export default props => {
+  const keyAdd = e => {
+    if (e.key === "Enter") {
+      props.formAdd();
+    }
+  };
+  return (
+    <div className="pb-2" role="form">
+      <div className="row">
+        <div className="col-9 col-md-10 col-lg-10 col-xl-10">
+          <input
+            id="description"
+            className="form-control"
+            placeholder="add item"
+            onChange={props.formChange}
+            value={props.description}
+            onKeyUp={keyAdd}
+          />
+        </div>
 
-      <div className="col-3 col-md-2 col-lg-2 col-xl-2">
-        <IconButton
-          color="primary"
-          icon="plus"
-          onClick={props.formAdd}
-          title="Add new item"
-        />
+        <div className="col-3 col-md-2 col-lg-2 col-xl-2">
+          <IconButton
+            color="primary"
+            icon="plus"
+            onClick={props.formAdd}
+            title="Add new item"
+          />
+        </div>
       </div>
     </div>
-  </div>
-);
+  );
+};
