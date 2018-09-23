9e3dd34dc739da9cb30dcd65bf49f78378e40eb5
Leandro Tavares <leandro.mendestavares@gmail.com>
2018-09-22 23:18:33 -0300

    Remove console logs

--- src/components/template/SelectForm.js
+++ src/components/template/SelectForm.js
@@ -3,7 +3,6 @@ import React from "react";
 
 // Component Stateless
 export default props => {
-  console.log(props.list);
   return (
     <select
       id="inputState"
@@ -11,9 +10,7 @@ export default props => {
       defaultValue=""
       placeholder="select the parent"
     >
-      <option disabled value="">
-        select the parent
-      </option>
+      <option value="">select the parent</option>
       {props.list.map(item => (
         <option key={item.id} value={item.id}>
           {item.name}
